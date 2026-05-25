e83b92f2d8832347d8b265ce59f689e9b4161a33
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-25 11:10:10 +0200

    FIX: Added missing id

---
+++ src/main/java/it/itsacademy/gestioneordinirestclient/dto/OrdineDTO.java
@@ -0,0 +1,22 @@
+package it.itsacademy.gestioneordinirestclient.dto;
+
+import it.itsacademy.gestioneordinirestclient.model.Ordine;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor @NoArgsConstructor
+public class OrdineDTO {
+    @Id private UUID idOrdine;
+    private Date dataCreazione;
+    private String descrizione;
+    private Ordine.StatoOrdine statoOrdine;
+    private Double totale;
+
+    public enum StatoOrdine {
+        DAPAGARE, PAGATO, ELIMINATO
+    }
+}
