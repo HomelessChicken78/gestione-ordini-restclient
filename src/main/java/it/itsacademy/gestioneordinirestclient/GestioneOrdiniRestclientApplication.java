5a617fb1572fa2d16ad14589ddf82a51057ccddf
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-25 10:44:41 +0200

    Initialized project

---
+++ src/main/java/it/itsacademy/gestioneordinirestclient/GestioneOrdiniRestclientApplication.java
@@ -0,0 +1,13 @@
+package it.itsacademy.gestioneordinirestclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneOrdiniRestclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestioneOrdiniRestclientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/itsacademy/gestioneordinirestclient/GestioneOrdiniRestclientApplicationTests.java
@@ -0,0 +1,13 @@
+package it.itsacademy.gestioneordinirestclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneOrdiniRestclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
