7f32b9851048488b9b7b28e46470f0f345bfba75
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-25 16:24:47 +0200

    FEAT: Added dto for the body of the other microservice

---
+++ src/main/java/it/itsacademy/gestioneordinirestclient/dto/CreaPagamentoDTO.java
@@ -0,0 +1,9 @@
+package it.itsacademy.gestioneordinirestclient.dto;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor @NoArgsConstructor
+public class CreaPagamentoDTO {
+    private Double totale = 0.0;
+}
