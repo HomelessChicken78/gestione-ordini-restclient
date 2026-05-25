c6e06b0be01da896d0dd2f8eb5d1f6ff6b498f2a
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-25 12:59:27 +0200

    FEAT: Added custom exceptions and handler

---
+++ src/main/java/it/itsacademy/gestioneordinirestclient/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package it.itsacademy.gestioneordinirestclient.exception;
+
+import it.itsacademy.gestioneordinirestclient.exception.dto.GeneralErrorResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity<GeneralErrorResponseDTO> error404Handler(NotFoundException err404) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND) // Usando .notFound non posso mettere un body con .body perché non ritorna
+                // un BodyBuilder (usato per il body) ma un HeadersBuilder (usato per gli headers)
+                .body(new GeneralErrorResponseDTO(err404.getMessage(), 404));
+    }
+}
---
+++ src/main/java/it/itsacademy/gestioneordinirestclient/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package it.itsacademy.gestioneordinirestclient.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
