78d186ba87079684c8c302a75388b3d500ec43f2
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-25 12:59:43 +0200

    FEAT: Added method to find an ordine or throw an exception

--- src/main/java/it/itsacademy/gestioneordinirestclient/repository/RepositoryOrdine.java
+++ src/main/java/it/itsacademy/gestioneordinirestclient/repository/RepositoryOrdine.java
@@ -1,9 +1,15 @@
 package it.itsacademy.gestioneordinirestclient.repository;
 
+import it.itsacademy.gestioneordinirestclient.exception.NotFoundException;
 import it.itsacademy.gestioneordinirestclient.model.Ordine;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface RepositoryOrdine extends JpaRepository<Ordine, UUID> {
+    default Ordine findByIdOrThrow(UUID idOrdine) {
+        return findById(idOrdine)
+                .orElseThrow(() -> new NotFoundException("Non esiste un ordine con id " + idOrdine));
+    }
 }
