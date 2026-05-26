311f6b2be45e0929ea8d10dec39fb4ca7033f611
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-26 09:33:17 +0200

    FEAT: Added method in repository to find all of the not deleted orders + minor refactor in findByIdOrThrow

--- src/main/java/it/itsacademy/gestioneordinirestclient/repository/RepositoryOrdine.java
+++ src/main/java/it/itsacademy/gestioneordinirestclient/repository/RepositoryOrdine.java
@@ -3,13 +3,18 @@ package it.itsacademy.gestioneordinirestclient.repository;
 import it.itsacademy.gestioneordinirestclient.exception.NotFoundException;
 import it.itsacademy.gestioneordinirestclient.model.Ordine;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-import java.util.Optional;
+import java.util.Collection;
 import java.util.UUID;
 
 public interface RepositoryOrdine extends JpaRepository<Ordine, UUID> {
     default Ordine findByIdOrThrow(UUID idOrdine) {
         return findById(idOrdine)
+                .filter(o -> o.getStatoOrdine() != Ordine.StatoOrdine.ELIMINATO)
                 .orElseThrow(() -> new NotFoundException("Non esiste un ordine con id " + idOrdine));
     }
+
+    @Query("SELECT o FROM Ordine AS o WHERE o.statoOrdine <> 'ELIMINATO'")
+    Collection<Ordine> findAllNotDeleted();
 }
