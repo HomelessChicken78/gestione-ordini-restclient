6775ae9fa05df81f213c43945bbac110b2126256
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-27 12:14:36 +0200

    FEAT: Now correctly manage status change of order

---
+++ src/main/java/it/itsacademy/gestioneordinirestclient/messaging/RicevitorePagamento.java
@@ -0,0 +1,42 @@
+package it.itsacademy.gestioneordinirestclient.messaging;
+
+import it.itsacademy.gestioneordinirestclient.model.Ordine;
+import it.itsacademy.gestioneordinirestclient.repository.RepositoryOrdine;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Component @Transactional
+@RequiredArgsConstructor
+public class RicevitorePagamento {
+    private final RepositoryOrdine repositoryOrdine;
+
+    @RabbitListener(queues = {"payments.success.queue"})
+    public void successfulPayment(UUID idOrdine) {
+        Ordine ordine = repositoryOrdine.findByIdOrThrow(idOrdine);
+
+        // Per sicurezza controlliamo che lo stato sia in elaborazione.
+        // Infatti AMQ garantisce che il messaggio sia mandato almeno una volta, ma nulla vieta che venga inviato due volte.
+        // NB: Non lanciamo eccezioni RabbitMQ penserebbe che ci sia stato un errore di elaborazione e rimetterebbe il messaggio in coda
+        if (ordine.getStatoOrdine() != Ordine.StatoOrdine.INELABORAZIONE)
+            return;
+
+        ordine.setStatoOrdine(Ordine.StatoOrdine.PAGATO);
+    }
+
+    @RabbitListener(queues = {"payments.failure.queue"})
+    public void failedPayment(UUID idOrdine) {
+        Ordine ordine = repositoryOrdine.findByIdOrThrow(idOrdine);
+
+        // Per sicurezza controlliamo che lo stato sia in elaborazione.
+        // Infatti AMQ garantisce che il messaggio sia mandato almeno una volta, ma nulla vieta che venga inviato due volte.
+        // NB: Non lanciamo eccezioni RabbitMQ penserebbe che ci sia stato un errore di elaborazione e rimetterebbe il messaggio in coda
+        if (ordine.getStatoOrdine() != Ordine.StatoOrdine.INELABORAZIONE)
+            return;
+
+        ordine.setStatoOrdine(Ordine.StatoOrdine.DAPAGARE);
+    }
+}
