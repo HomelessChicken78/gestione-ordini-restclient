a6327bf8b4facd8845e0d347cb27ab4240a3ba3c
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-27 12:36:31 +0200

    CHORE: Uses jackson instead of simplemessageconverter

--- src/main/java/it/itsacademy/gestioneordinirestclient/config/RabbitMQConfiguration.java
+++ src/main/java/it/itsacademy/gestioneordinirestclient/config/RabbitMQConfiguration.java
@@ -1,6 +1,8 @@
 package it.itsacademy.gestioneordinirestclient.config;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -97,5 +99,17 @@ public class RabbitMQConfiguration {
                 .to(exchange)
                 .with("payments.rifiutato")
                 .noargs();
+    }
+
+    /*
+    Contesto: Di default spring per convertire una stringa in json e viceversa usa un SimpleMessageConverter
+    (eccezione fatta per una rest controller), che vuole una stringa e converte in un array di byte e viceversa,
+    ma non è compatibile con gli oggetti.
+    Jackson invece è un MessageConverter che converte gli oggetti in json direttamente.
+    Con questo bean diciamo a spring: "Non usare il tuo SimpleMessageConverter che è vecchio. Usa Jackson".
+     */
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new JacksonJsonMessageConverter();
     }
 }
