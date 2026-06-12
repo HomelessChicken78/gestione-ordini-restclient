21c999f5373c62b38d8c34c9a5f9954e87372c6d
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-06-12 12:44:20 +0200

    REFACTOR: Sending email is now asynchronous

--- src/main/java/it/itsacademy/gestioneordinirestclient/messaging/RicevitoreMail.java
+++ src/main/java/it/itsacademy/gestioneordinirestclient/messaging/RicevitoreMail.java
@@ -3,19 +3,27 @@ package it.itsacademy.gestioneordinirestclient.messaging;
 import it.itsacademy.gestioneordinirestclient.dto.OrderPaymentEmailEvent;
 import it.itsacademy.gestioneordinirestclient.service.EmailService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
-@RequiredArgsConstructor
+@Component @Transactional
+@RequiredArgsConstructor @Slf4j
 public class RicevitoreMail {
     private final EmailService email;
 
     @RabbitListener(queues = {"payment.success.email"})
     public void sendPaymentSuccessEmail(OrderPaymentEmailEvent emailEvent) {
+        log.info("Sending success email. recipient={}", emailEvent.getEmail());
         email.sendOrderPaymentSuccessMail(emailEvent.getOrderId(), emailEvent.getEmail(), emailEvent.getOrderDescription());
+        log.debug("Sent email. recipient={}", emailEvent.getEmail());
     }
 
     @RabbitListener(queues = {"payment.failure.email"})
     public void sendPaymentFailureEmail(OrderPaymentEmailEvent emailEvent) {
+        log.info("Sending failure email. recipient={}", emailEvent.getEmail());
         email.sendOrderPaymentFailMail(emailEvent.getOrderId(), emailEvent.getEmail(), emailEvent.getOrderDescription());
+        log.debug("Sent email. recipient={}", emailEvent.getEmail());
     }
 }
--- src/main/java/it/itsacademy/gestioneordinirestclient/messaging/RicevitorePagamento.java
+++ src/main/java/it/itsacademy/gestioneordinirestclient/messaging/RicevitorePagamento.java
@@ -1,12 +1,13 @@
 package it.itsacademy.gestioneordinirestclient.messaging;
 
+import it.itsacademy.gestioneordinirestclient.dto.OrderPaymentEmailEvent;
+import it.itsacademy.gestioneordinirestclient.exception.NotFoundException;
 import it.itsacademy.gestioneordinirestclient.model.Ordine;
 import it.itsacademy.gestioneordinirestclient.repository.RepositoryOrdine;
-import it.itsacademy.gestioneordinirestclient.service.EmailService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.mail.MailException;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,11 +17,11 @@ import java.util.UUID;
 @RequiredArgsConstructor @Slf4j
 public class RicevitorePagamento {
     private final RepositoryOrdine repositoryOrdine;
-    private final EmailService email;
+    private final RabbitTemplate rabbitTemplate;
 
     @RabbitListener(queues = {"payments.success.queue"})
     public void successfulPayment(UUID idOrdine) {
-        Ordine ordine = repositoryOrdine.findByIdOrThrow(idOrdine);
+        Ordine ordine = findByIdOrLogAndThrow(idOrdine);
 
         // Per sicurezza controlliamo che lo stato sia in elaborazione.
         // Infatti AMQ garantisce che il messaggio sia mandato almeno una volta, ma nulla vieta che venga inviato due volte.
@@ -28,18 +29,15 @@ public class RicevitorePagamento {
         if (ordine.getStatoOrdine() != Ordine.StatoOrdine.IN_ELABORAZIONE)
             return;
 
-        try {
-            email.sendOrderPaymentSuccessMail(ordine.getIdOrdine(), ordine.getEmailCliente(), ordine.getDescrizione());
-        } catch (MailException e) {
-            log.error(e.getMessage());
-        }
+        rabbitTemplate.convertAndSend("payments.exchange", "email.payment.accettato",
+                new OrderPaymentEmailEvent(ordine.getIdOrdine(), ordine.getEmailCliente(), ordine.getDescrizione()));
 
         ordine.setStatoOrdine(Ordine.StatoOrdine.PAGATO);
     }
 
     @RabbitListener(queues = {"payments.failure.queue"})
     public void failedPayment(UUID idOrdine) {
-        Ordine ordine = repositoryOrdine.findByIdOrThrow(idOrdine);
+        Ordine ordine = findByIdOrLogAndThrow(idOrdine);
 
         // Per sicurezza controlliamo che lo stato sia in elaborazione.
         // Infatti AMQ garantisce che il messaggio sia mandato almeno una volta, ma nulla vieta che venga inviato due volte.
@@ -47,12 +45,18 @@ public class RicevitorePagamento {
         if (ordine.getStatoOrdine() != Ordine.StatoOrdine.IN_ELABORAZIONE)
             return;
 
-        try {
-            email.sendOrderPaymentFailMail(ordine.getIdOrdine(), ordine.getEmailCliente(), ordine.getDescrizione());
-        } catch (MailException e) {
-            log.error(e.getMessage());
-        }
+        rabbitTemplate.convertAndSend("payments.exchange", "email.payment.rifiutato",
+                new OrderPaymentEmailEvent(ordine.getIdOrdine(), ordine.getEmailCliente(), ordine.getDescrizione()));
 
         ordine.setStatoOrdine(Ordine.StatoOrdine.DA_PAGARE);
+    }
+
+    private Ordine findByIdOrLogAndThrow(UUID idOrdine) {
+        try {
+            return repositoryOrdine.findByIdOrThrow(idOrdine);
+        } catch (NotFoundException e) {
+            log.warn("Order not found. id={}", idOrdine);
+            throw e;
+        }
     }
 }
