9f7c34cd94da6e2fdf38cc6a73804f48a151371f
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-06-24 10:14:37 +0200

    CHORE: configured s3 bean for spring

--- src/main/java/it/itsacademy/gestioneordinirestclient/config/GeneralConfigurations.java
+++ src/main/java/it/itsacademy/gestioneordinirestclient/config/GeneralConfigurations.java
@@ -4,6 +4,11 @@ import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+import java.time.Duration;
 
 @Configuration
 public class GeneralConfigurations {
@@ -17,5 +22,16 @@ public class GeneralConfigurations {
     @Bean
     public MessageConverter jsonMessageConverter() {
         return new JacksonJsonMessageConverter();
+    }
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                // Non serve mettere il Credential Provider, perchè prende quello definito sul vostro PC tramite AWS CLI
+                //.credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
+                // .credentialsProvider(DefaultCredentialsProvider.create()) NB: Non serve: lo fa da solo
+                //Tempo di attesa massima per l'interazione con AWS
+                .overrideConfiguration(b -> b.apiCallTimeout(Duration.ofMinutes(2)))
+                .build();
     }
 }
